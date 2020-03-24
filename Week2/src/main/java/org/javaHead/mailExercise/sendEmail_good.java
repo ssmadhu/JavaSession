f74bcf779265a2531e5ec2d75edae53e5f464c2c
Madhumitha Sekar <ssmadhumitha@gmail.com>
2020-03-24 16:43:33 -0500

    Update pom.xml for build as jar with dependencies

--- Week2/src/main/java/org/javaHead/mailExercise/sendEmail_good.java
+++ Week2/src/main/java/org/javaHead/mailExercise/sendEmail_good.java
@@ -14,7 +14,7 @@ public class sendEmail_good
     {
         EmailService emailService = new EmailService();
         EmailInfo emailInfo = new EmailInfo("Hi!", "Have a good day");
-        User from = new User("Madhumitha", "Sekar", "Madhumitha Sekar", "****");
+        User from = new User("Madhumitha", "Sekar", "ssmadhumitha@gmail.com", "****");
         User to = new User("Akshay", "Sharma", "asharma2023@gmail.com");
         emailService.sendEmail(emailInfo, from, to);
     }
